fe0c65ca9dec82e47abfba18117bbe6df6cafcf1
DAJEONGHADA <112385274+DAJEONGHADA@users.noreply.github.com>
2024-11-21 14:26:15 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,3 +1,13 @@
-SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME IN ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty")
+WHERE SEX_UPON_INTAKE LIKE '%Spayed%'
+        AND NAME = 'Lucy'
+        OR NAME = 'Ella'
+        OR NAME = 'Pickle'
+        OR NAME = 'Rogan'
+        OR NAME = 'Sabrina'
+        OR NAME = 'Mitty'
+ORDER BY ANIMAL_ID ASC;
